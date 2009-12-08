1f535e7d7fd580a11591be9fd351e632d4479d33
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-07 19:45:42 -0500

    Promoted Result to an interface. Added variant Result interfaces as well. Clarified the README a little.

--- db.go
+++ db.go
@@ -135,10 +135,45 @@ type Connection interface {
 	Close() os.Error;
 }
 
-// The iterator approach to execute returns these things
-type Result struct {
-	Data	[]interface{};
-	Error	os.Error;
+// The most basic type of result.
+// 
+// Data() returns the data for this result as an array
+// of generic objects. The database driver in question
+// defines what concrete types are returned depending
+// on the types used by the database system.
+//
+// Error() returns the error that occurred when this
+// result was fetched, or nil if no error occurred.
+type Result interface {
+	Data() []interface{};
+	Error() os.Error;
+}
+
+// InformativeResults supply useful but optional information.
+//
+// Fields() returns the names of each item of data in the
+// result.
+//
+// Types() returns the names of the types of each item in
+// the result.
+type InformativeResult interface {
+	Result;
+	Fields() []string;
+	Types() []string;
+}
+
+// FancyResults provide an alternate way of processing results.
+//
+// DataMap() returns a map from item names to item values. As
+// for Data() the concrete types have to be defined by the
+// database driver in question.
+//
+// TypeMap() returns a map from item names to the names of the
+// types of each item.
+type FancyResult interface {
+	Result;
+	DataMap() map[string]interface{};
+	TypeMap() map[string]string;
 }
 
 // InformativeConnections supply useful but optional information.
@@ -238,9 +273,13 @@ type PythonicCursor interface {
 }
 
 // ExecuteDirectly is a convenience function for "one-off" queries.
+// It's particularly convenient for queries that don't produce any
+// results.
+//
 // If you need more control, for example to rebind parameters over
-// and over again, or to get results one by one, you should use the
-// Prepare() and Execute() methods explicitly instead.
+// and over again, to get results one by one, or to access metadata
+// about the results, you should use the Prepare() and Execute()
+// methods explicitly instead.
 func ExecuteDirectly(conn Connection, query string, params ...) (results [][]interface{}, err os.Error) {
 	var s Statement;
 	s, err = conn.Prepare(query);
