c29786e9be52d05fb99629160631a9d8422e6f30
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-11 02:34:06 -0500

    Extracted "result" API to its own file.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 include $(GOROOT)/src/Make.$(GOARCH)
 
 TARG=db
-GOFILES=db.go classic.go util.go
+GOFILES=db.go classic.go util.go result.go
 
 include $(GOROOT)/src/Make.pkg
---
+++ result.go
@@ -0,0 +1,48 @@
+// Copyright 2009 Peter H. Froehlich. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package db
+
+import "os"
+
+// The most basic type of result.
+//
+// Data() returns the data for this result as an array
+// of generic objects. The database driver in question
+// defines what concrete types are returned depending
+// on the types used by the database system.
+//
+// Error() returns the error that occurred when this
+// result was fetched, or nil if no error occurred.
+type Result interface {
+	Data() []interface{};
+	Error() os.Error;
+}
+
+// InformativeResults supply useful but optional information.
+//
+// Fields() returns the names of each item of data in the
+// result.
+//
+// Types() returns the names of the types of each item in
+// the result.
+type InformativeResult interface {
+	Result;
+	Fields() []string;
+	Types() []string;
+}
+
+// FancyResults provide an alternate way of processing results.
+//
+// DataMap() returns a map from item names to item values. As
+// for Data() the concrete types have to be defined by the
+// database driver in question.
+//
+// TypeMap() returns a map from item names to the names of the
+// types of each item.
+type FancyResult interface {
+	Result;
+	DataMap() map[string]interface{};
+	TypeMap() map[string]string;
+}
