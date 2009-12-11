d0160a8b1deb8b2e5f91035c02ffb9c17f54acb4
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-11 15:46:47 -0500

    Added documentation.

--- set.go
+++ set.go
@@ -6,7 +6,15 @@ package db
 
 import "os"
 
-// TODO: the new way of returning results from Execute()
+// The simplest way of processing query results.
+//
+// Iter() returns a channel of Result objects which can be
+// examined one at a time. Note that called Iter() several
+// times will return the same channel over and over again.
+//
+// Close() shuts down the iteration mechanism and frees all
+// associated resources. After a result set has been closed,
+// Iter() will return nil.
 type ResultSet interface {
 	Iter() <-chan Result;
 	Close() os.Error;
